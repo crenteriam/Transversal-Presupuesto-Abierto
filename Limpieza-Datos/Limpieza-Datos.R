6aed5c403d9adf954f6d744fb6829ff6e4afbdaa
crenteriam <renteman@hotmail.com>
2019-09-25 16:03:26 -0500

    datos

---
+++ Limpieza-Datos/Limpieza-Datos.R
@@ -0,0 +1,13 @@
+
+# Vinculo de trabajo:
+#  https://presupuestociudadano.jalisco.gob.mx/presupuesto
+
+
+# Tareas para Giovanny
+
+
+# 1. Limpiar: Por Unidad Presupuestal - Unidad Responsable y Programa Presupuestario
+# Y guardarla como .csv
+# Hacer eso para 2018 y 2019
+# posteriormente combinar 2018 y 2018 en una sola base de datos y guargar en .csv
+

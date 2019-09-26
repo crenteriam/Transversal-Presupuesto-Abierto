2d024626c546bad7cee6618bd2edd07930ab340a
crenteriam <renteman@hotmail.com>
2019-09-26 11:21:02 -0500

    datos

--- Limpieza-Datos/Limpieza-Datos.R
+++ Limpieza-Datos/Limpieza-Datos.R
@@ -1,13 +1,16 @@
-
-# Vinculo de trabajo:
+# Vinculo para acceder a las bases de datos.
 #  https://presupuestociudadano.jalisco.gob.mx/presupuesto
 
 
-# Tareas para Giovanny
-
-
-# 1. Limpiar: Por Unidad Presupuestal - Unidad Responsable y Programa Presupuestario
+# 1. Limpiar: Por Unidad Presupuestal - Unidad Responsable y Programa Presupuestario (usa el archivo programa-presupuestario-2018.csv)
 # Y guardarla como .csv
-# Hacer eso para 2018 y 2019
-# posteriormente combinar 2018 y 2018 en una sola base de datos y guargar en .csv
 
+# hacer lo mismo para la base de datos 2019  (usa el archivo programa-presupuestario-2018.csv).
+
+# 3. Hacer eso para 2018 y 2019
+# posteriormente combinar 2018 y 2018 en una sola base de datos y guargar en .csv
+# para hacer el append, usa el comando rbind() o bind_row(). Son diferentes, pero hacen lo mismo. El que te sirva mas.
+# para hacer el append, necesitas primero hacer una variable year para poner el año
+
+#####################################################################################################
+# Empezar la programacion aqui abajo.

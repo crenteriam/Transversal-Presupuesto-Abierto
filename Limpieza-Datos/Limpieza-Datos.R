b798c4287c08a64b63d576290002f3abb4a74db7
GiovannyA <amador.giovanny@gmail.com>
2019-10-02 19:10:22 -0500

    Append

--- Limpieza-Datos/Limpieza-Datos.R
+++ Limpieza-Datos/Limpieza-Datos.R
@@ -36,4 +36,14 @@ lapply(Packages, library, character.only = TRUE)
 #####################################################################################################
 # Empezar la programacion aqui abajo.
 
+egresos_2018 <-"Presupuesto-Jalisco/2018-egresos-autorizado/egresos 2018.csv"
+egresos_2019 <-"Presupuesto-Jalisco/2019-egresos-autorizado/egresos 2019.csv"
+egresos <-rbind(egresos_2018,egresos_2019)
+
+ingresos_2018 <-"Presupuesto-Jalisco/2018-ingresos-autorizado/Ingresos 2018.csv"
+ingresos_2019 <-"Presupuesto-Jalisco/2019-ingresos-autorizado/Ingresos 2019.csv"
+ingresos <- rbind(ingresos_2018,ingresos_2019)
+
+
+
 

d6277b86758fad12e9985b61786b9491bbf2e007
GiovannyA <amador.giovanny@gmail.com>
2019-10-15 11:16:58 -0500

    Limpieza datos

--- Limpieza-Datos/Limpieza-Datos.R
+++ Limpieza-Datos/Limpieza-Datos.R
@@ -18,23 +18,6 @@ Packages <- c("haven", "tidyverse")
 #install.packages(Packages)
 lapply(Packages, library, character.only = TRUE)
 
-
-# Vinculo para acceder a las bases de datos.
-#  
-
-
-# 1. Limpiar: Por Unidad Presupuestal - Unidad Responsable y Programa Presupuestario (usa el archivo programa-presupuestario-2018.csv)
-# Y guardarla como .csv para el viernes
-
-# hacer lo mismo para la base de datos 2019  (usa el archivo programa-presupuestario-2018.csv).
-# para el lunes
-
-# 3. Hacer eso para 2018 y 2019
-# posteriormente combinar 2018 y 2018 en una sola base de datos y guargar en .csv
-# para hacer el append, usa el comando rbind() o bind_row(). Son diferentes, pero hacen lo mismo. El que te sirva mas.
-# para hacer el append, necesitas primero hacer una variable year para poner el año
-# para el martes
-
 #####################################################################################################
 # Empezar la programacion aqui abajo.
 

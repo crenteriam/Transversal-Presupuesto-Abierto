f5761501bb87bb9f54c47c2cf8511a2aa69ba187
GiovannyA <amador.giovanny@gmail.com>
2019-11-12 16:23:05 -0600

    Gráfica de serie de tiempo presupuesto

---
+++ Presupuesto-Jalisco/Presupuesto serie de tiempo/Serie de tiempo.R
@@ -0,0 +1,7 @@
+library(ggplot2)
+library(dplyr)
+library(hrbrthemes)
+
+ggplot(Presupuesto, aes(x=year, y=`monto(mdp)`)+geom_line(color="steelblue")+geom_point()+xlab("Presupuesto a través del tiempo")+ylab("Millones de pesos")+theme_ipsum()+theme(axis.text.x=element_text(angle=60, hjust=1))+scale_y_continuous(labels=function(x) format(x, big.mark = ","))+theme(axis.text.x=element_text(size=11, angle=90,hjust=0.95,vjust=0.2))
+    
+       

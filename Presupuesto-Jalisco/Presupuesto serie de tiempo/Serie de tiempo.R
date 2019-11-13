df612aed7a4938eedf39904440f05cf775f94442
GiovannyA <amador.giovanny@gmail.com>
2019-11-13 17:55:59 -0600

    l

--- Presupuesto-Jalisco/Presupuesto serie de tiempo/Serie de tiempo.R
+++ Presupuesto-Jalisco/Presupuesto serie de tiempo/Serie de tiempo.R
@@ -16,8 +16,16 @@ lapply(Packages, library, character.only = TRUE)
 Presupuesto <- read_csv("Presupuesto-Jalisco/Presupuesto serie de tiempo/Presupuesto.csv")
 View(Presupuesto)
 #Gráfica valor nominal
-ggplot(Presupuesto, aes(x=year, y=`monto(mdp)`))+geom_line(color="dark blue")+geom_point()+xlab("Presupuesto a través del tiempo")+ylab("Millones de pesos")+theme_ipsum()+theme(axis.text.x=element_text(angle=60, hjust=1))+scale_y_continuous(labels=function(x) format(x, big.mark = ","))+theme(axis.text.x=element_text(size=11, angle=90,hjust=0.95,vjust=0.2))+geom_point(shape=21,fill="dark blue", color="black", size=6)+ggtitle("El presupuesto a través del tiempo. Valor nominal")
+ggplot(Presupuesto, aes(x=year, y=`monto(mdp)`))+geom_line(color="dark blue")+
+  geom_point()+xlab("Año")+ylab("Millones de pesos")+theme_ipsum()+
+  theme(axis.text.x=element_text(angle=60, hjust=1))+scale_y_continuous(labels=function(x) format(x, big.mark = ","))+
+  theme(axis.text.x=element_text(size=11, angle=90,hjust=0.95,vjust=0.2))+geom_point(shape=21,fill="dark blue", color="black", size=6)+
+  ggtitle("El presupuesto a través del tiempo. Valor nominal")+theme(plot.title = element_text(hjust = 0.5))
 #Gráfica valor real
-ggplot(Presupuesto, aes(x=year, y=`monto_real(mdp)`))+geom_line(color="steel blue")+geom_point()+xlab("Presupuesto a través del tiempo")+ylab("Millones de pesos")+theme_ipsum()+theme(axis.text.x=element_text(angle=60, hjust=1))+scale_y_continuous(labels=function(x) format(x, big.mark = ","))+theme(axis.text.x=element_text(size=11, angle=90,hjust=0.95,vjust=0.2))+geom_point(shape=21,fill="steel blue", color="black", size=6)+ggtitle("El presupuesto a través del tiempo. Valor real")
+ggplot(Presupuesto, aes(x=year, y=`monto_real(mdp)`))+geom_line(color="steel blue")+
+  geom_point()+xlab("Año")+ylab("Millones de pesos(pesos 2018)")+theme_ipsum()+
+  theme(axis.text.x=element_text(angle=60, hjust=0.5))+scale_y_continuous(labels=function(x) format(x, big.mark = ","))+
+  theme(axis.text.x=element_text(size=11, angle=90,hjust=0.5,vjust=0.2))+geom_point(shape=21,fill="steel blue", color="black", size=6)+
+  ggtitle("El presupuesto a través del tiempo. Valor real")+theme(plot.title = element_text(hjust = 0.5))
 
-
+help("ylab")

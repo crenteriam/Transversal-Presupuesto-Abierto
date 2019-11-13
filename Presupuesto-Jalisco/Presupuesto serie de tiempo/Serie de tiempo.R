18b234bf92d016c1e85aab04f96509a189d97195
GiovannyA <amador.giovanny@gmail.com>
2019-11-13 16:45:15 -0600

    l

--- Presupuesto-Jalisco/Presupuesto serie de tiempo/Serie de tiempo.R
+++ Presupuesto-Jalisco/Presupuesto serie de tiempo/Serie de tiempo.R
@@ -13,6 +13,8 @@ Packages <- c("haven", "tidyverse","dplyr", "ggplot2", "hrbrthemes")
 #install.packages(Packages)
 lapply(Packages, library, character.only = TRUE)
 --------------------------------------------------------------------------------------------------
+Presupuesto <- read_csv("Presupuesto-Jalisco/Presupuesto serie de tiempo/Presupuesto.csv")
+View(Presupuesto)
 #Gráfica valor nominal
 ggplot(Presupuesto, aes(x=year, y=`monto(mdp)`))+geom_line(color="dark blue")+geom_point()+xlab("Presupuesto a través del tiempo")+ylab("Millones de pesos")+theme_ipsum()+theme(axis.text.x=element_text(angle=60, hjust=1))+scale_y_continuous(labels=function(x) format(x, big.mark = ","))+theme(axis.text.x=element_text(size=11, angle=90,hjust=0.95,vjust=0.2))+geom_point(shape=21,fill="dark blue", color="black", size=6)+ggtitle("El presupuesto a través del tiempo. Valor nominal")
 #Gráfica valor real
